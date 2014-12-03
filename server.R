9f02c0cd65c1c4c7f273ca3d4363853fef44af13
pmoulos@gmail.com <pmoulos@gmail.com@236c02bb-0a49-8157-473e-e659b052478f>
2014-12-03 15:33:15 +0000

    Getting grasps of Shiny and continuing UI

--- server.R
+++ server.R
@@ -2,10 +2,11 @@
 
 # Load required libraries
 library(shiny)
-#library(GenomicAlignments)
+library(GenomicAlignments)
 #library(HilbertVis)
 
-library(datasets)
+# Load additional functions
+source("helpers.R")
 
 # Set some global shiny options
 # Increase max upload size so as to accomodate BAM files
@@ -15,7 +16,10 @@ shinyServer(
     function(input,output,session) {
         
         # Intitiate list of observers
-        observers <- list()
+        session$observers <- list()
+        # Record data sources
+        session$bam <- initDataSource("bam")
+        session$link <- initDataSource("link")
         # Record data source changes
         session$bamHitCount <- session$linkHitCount <- 0
         # Record displayed messages
@@ -24,6 +28,7 @@ shinyServer(
         # Set up response to clear uploaded file buttons
         output$bam_1 <- renderUI({
             input$clear_bam_1
+            clearDataSource(session,"bam",1)
             fileInput(
                 inputId="bam_1",label=""
             )
@@ -42,15 +47,15 @@ shinyServer(
         })
         
         # Setup clear link button listeners (observers in shiny terms)
-        observers$link_1 <- observe({
+        session$observers$link_1 <- observe({
             input$clear_link_1
             updateTextInput(session,"link_1",value="")
         })
-        observers$link_2 <- observe({
+        session$observers$link_2 <- observe({
             input$clear_link_2
             updateTextInput(session,"link_2",value="")
         })
-        observers$link_3 <- observe({
+        session$observers$link_3 <- observe({
             input$clear_link_3
             updateTextInput(session,"link_3",value="")
         })
@@ -60,208 +65,170 @@ shinyServer(
             selectInput(
                 inputId="chromosomes",
                 label="",
-                choices=get.valid.chrs(input$organism)
+                choices=getValidChromosomes(input$organism)
             )
         })
         
         # TODO: Write a response to color selection, must convert to R colors
         
+        # Initialize the image area, co-ordinates and messages divs
+        initImage(output)
+        initCoords(output)
+        session$messages <- updateMessages(output,session,paste(
+            "What a nice shiny day on the hill! Let's analyze some data!"))
+        
         # Check if we have data to read in from BAM files
-        observers$source_input <- observe({
+        session$observers$source_input <- observe({
             input$go_bam
             input$go_link
             bam <- input$go_bam
             link <- input$go_link
-            if (bam==0 && link==0) { 
-                session$messages <- c(session$messages,
-                    paste("What a nice shiny day on the hill! ",
-                        "Let's analyze some data!"))
-            }
-            else {
-                if (bam > session$bamHitCount) {
-                    session$bamHitCount <- session$bamHitCount + 1
-                    isolate({
-                        bams.present <- logical(3)
-                        bam.paths <- bam.names <- rep(NA,3)
-                        if (is.null(input$bam_1$datapath)
-                            && is.null(input$bam_2$datapath)
-                            && is.null(input$bam_3$datapath)) {
-                            if (bam==1)
-                                session$messages <-
-                                    "No input BAM files were provided"
-                            else
-                                session$messages <- c(session$messages,
-                                    "No input BAM files were provided")
-                        }
-                        else {
-                            if (!is.null(input$bam_1$datapath)) {
-                                bams.present[1] <- TRUE
-                                bam.paths[1] <- input$bam_1$datapath
-                                bam.names[1] <- input$bam_1$name
-                            }
-                            if (!is.null(input$bam_2$datapath)) {
-                                bams.present[2] <- TRUE
-                                bam.paths[2] <- input$bam_2$datapath
-                                bam.names[2] <- input$bam_2$name
-                            }
-                            if (!is.null(input$bam_3$datapath)) {
-                                bams.present[3] <- TRUE
-                                bam.paths[3] <- input$bam_3$datapath
-                                bam.names[3] <- input$bam_3$name
-                            }
-                            
-                            reads <- coverage <- vector("list",
-                                length(bam.paths))
-                            names(reads) <- names(coverage) <- bam.names
-                            for (n in names(reads)) {
-                                paste("Reading total reads for ",n,"...")
-                                reads[[n]] <- as(readGAlignments(file=input[n]),"GRanges")
-                                disp("Calculating coverage for ",n,"...")
-                                coverage[[n]] <- coverage(reads[[n]])
-                            }
-                                
-                            session$messages <- c(session$messages,
-                                "Something was provided")
-                        }
-                            
-                    })
-                }
-                if (link > session$linkHitCount) {
-                    session$linkHitCount <- session$linkHitCount + 1
-                    isolate(
-                        if (input$link_1=="" && input$link_2==""
-                            && input$link_3=="") {
-                            if (link==1)
-                                session$messages <- 
-                                    "No links to indexed BAM files were provided"
-                            else
-                                session$messages <- c(session$messages,
-                                    "No links to indexed BAM files were provided")
-                        }
+
+            if (bam > session$bamHitCount) {
+                session$bamHitCount <- session$bamHitCount + 1
+                isolate({
+                    bams.present <- logical(3)
+                    bam.paths <- bam.names <- rep(NA,3)
+                    if (is.null(input$bam_1$datapath)
+                        && is.null(input$bam_2$datapath)
+                        && is.null(input$bam_3$datapath)) {
+                        if (bam==1)
+                            session$messages <- updateMessages(output,
+                                session,"No input BAM files were provided",
+                                clear=TRUE)
                         else
-                            session$messages <- c(session$messages,
-                                "Something was provided")
-                    )
-                }
+                            session$messages <- updateMessages(output,
+                                session,"No input BAM files were provided")
+                    }
+                    else {
+                        if (!is.null(input$bam_1$datapath)) {
+                            bams.present[1] <- TRUE
+                            bam.paths[1] <- input$bam_1$datapath
+                            bam.names[1] <- input$bam_1$name
+                        }
+                        if (!is.null(input$bam_2$datapath)) {
+                            bams.present[2] <- TRUE
+                            bam.paths[2] <- input$bam_2$datapath
+                            bam.names[2] <- input$bam_2$name
+                        }
+                        if (!is.null(input$bam_3$datapath)) {
+                            bams.present[3] <- TRUE
+                            bam.paths[3] <- input$bam_3$datapath
+                            bam.names[3] <- input$bam_3$name
+                        }
+                        bam.paths <- bam.paths[which(bams.present)]
+                        bam.names <- bam.names[which(bams.present)]
+                        names(bam.paths) <- bam.names
+                        
+                        reads <- coverage <- vector("list",
+                            length(bam.paths))
+                        names(reads) <- names(coverage) <- bam.names
+                        for (n in names(reads)) {
+                            session$messages <- updateMessages(output,
+                                session,paste("Reading total reads for ",n,
+                                    "...",sep=""))
+                            reads[[n]] <- as(readGAlignments(file=bam.paths[n]),
+                                "GRanges")
+                            session$messages <- updateMessages(output,
+                                session,paste("Calculating coverage for ",n,
+                                    "...",sep=""))
+                            coverage[[n]] <- coverage(reads[[n]])
+                        }
+                            
+                        session$messages <- updateMessages(output,session,
+                            "Done!")
+                    }
+                        
+                })
             }
-            
-            output$messages <- renderUI({
-                tags$div(
-                    isolate(
-                        HTML(paste(session$messages,collapse="<br/>"))
-                    )
+
+            if (link > session$linkHitCount) {
+                session$linkHitCount <- session$linkHitCount + 1
+                isolate(
+                    if (input$link_1=="" && input$link_2==""
+                        && input$link_3=="") {
+                        if (link==1)
+                           session$messages <- updateMessages(output,session,
+                                "No links to indexed BAM files were provided")
+                        else
+                            session$messages <- updateMessages(output,session,
+                                "No links to indexed BAM files were provided")
+                    }
+                    else
+                        session$messages <- updateMessages(output,session,
+                            "Something was provided")
                 )
-            })
-        })
-        
-        # Fill in the spot we created for a plot
-        output$hilbert <- renderPlot({
-            # Initialize the visualization environment
-            plot(1:100,1:100,xaxt="n",yaxt="n",pch=20,col="white",
-                xlab="",ylab="")
-            text(50,50,"The image will be displayed here",cex=5,font=2)
-            
-            output$coords <- renderText({
-                "Chromosomal coordinates will be displayed here"
-            })
-        })
-        
-        output$browser <- renderUI({
-            tags$div(
-                "Associated UCSC Genome Browser session",
-                tags$br(),
-                tags$a(
-                    href="http://genome.ucsc.edu",
-                    target="_blank",
-                    "http://genome.ucsc.edu"
-                )
-            )
+            }
         })
         
         # When the client ends the session, suspend the observers
-        session$onSessionEnded(function() {
-            for (obs in names(observers))
-                observers[[obs]]$suspend()
-        })
+        destroyObservers(session)
     }
 )
 
-get.valid.chrs <- function(org) {
-    switch(org,
-        hg18 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr19","chr2","chr20","chr21","chr22","chr3",
-                "chr4","chr5","chr6","chr7","chr8","chr9","chrX","chrY"
+clearDataSource <- function(session) {
+    session$bam[[1]]$present <- FALSE
+    session$path[[1]]$path <- NA
+    session$bam[[1]]$present <- FALSE
+}
+
+updateMessages <- function(output,session,msg,clear=FALSE) {
+    if (clear)
+        session$messages <- msg
+    else
+        session$messages <- c(session$messages,msg)
+    output$messages <- renderUI({
+        tags$div(
+            #isolate(
+                HTML(paste(session$messages,collapse="<br/>"))
+            #)
+        )
+    })
+    return(session$messages)
+}
+
+initImage <- function(output) {
+    output$hilbert <- renderPlot({
+        plot(1:100,1:100,xaxt="n",yaxt="n",pch=20,col="white",
+            xlab="",ylab="")
+        text(50,50,"The image will be displayed here",cex=5,font=2)
+        
+        output$coords <- renderText({
+            "Chromosomal coordinates will be displayed here"
+        })
+    })
+}
+
+initCoords <- function(output) {
+    output$browser <- renderUI({
+        tags$div(
+            "Associated UCSC Genome Browser session",
+            tags$br(),
+            tags$a(
+                href="http://genome.ucsc.edu",
+                target="_blank",
+                "http://genome.ucsc.edu"
+            )
+        )
+    })
+}
+
+initDataSource <- function(srctype) {
+    switch(srctype,
+        bam = {
+            return(list(
+                present <- logical(3),
+                path <- rep(NA,3),
+                name <- rep(NA,3)
             ))
         },
-        hg19 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr19","chr2","chr20","chr21","chr22","chr3",
-                "chr4","chr5","chr6","chr7","chr8","chr9","chrX","chrY"
-            ))
-        },
-        hg38 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr19","chr2","chr20","chr21","chr22","chr3",
-                "chr4","chr5","chr6","chr7","chr8","chr9","chrX","chrY"
-            ))
-        },
-        mm9 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr19","chr2","chr3","chr4","chr5","chr6",
-                "chr7","chr8","chr9","chrX","chrY"
-            ))
-        },
-        mm10 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr19","chr2","chr3","chr4","chr5","chr6",
-                "chr7","chr8","chr9","chrX","chrY"
-            ))
-        },
-        rn5 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr19","chr2","chr3","chr4","chr5","chr6",
-                "chr7","chr8","chr9","chrX"
-            ))
-        },
-        dm3 = {
-            return(c(
-                "chr2L","chr2LHet","chr2R","chr2RHet","chr3L","chr3LHet",
-                "chr3R","chr3RHet","chr4","chrU","chrUextra","chrX","chrXHet",
-                "chrYHet"
-            ))
-        },
-        danRer7 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr19","chr2","chr20","chr21","chr22","chr23",
-                "chr24","chr25","chr3","chr4","chr5","chr6","chr7","chr8","chr9"
-            ))
-        },
-        panTro4 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr19","chr20","chr21","chr22","chr2A","chr2B",
-                "chr3","chr4","chr5","chr6","chr7","chr8","chr9","chrX","chrY"
-            ))
-        },
-        susScr3 = {
-            return(c(
-                "chr1","chr10","chr11","chr12","chr13","chr14","chr15","chr16",
-                "chr17","chr18","chr2","chr3","chr4","chr5","chr6","chr7",
-                "chr8","chr9","chrX","chrY"
-            ))
-        },
-        tair10 = {
-            return(c(
-                "chr1","chr2","chr3","chr4","chr5"
-            ))
-        }
+        link = {}
     )
+}
+
+destroyObservers <- function(session) {
+    session$onSessionEnded(function() {
+        for (obs in names(session$observers))
+            session$observers[[obs]]$suspend()
+    })
 }
